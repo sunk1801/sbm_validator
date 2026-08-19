8589dec13efa17734bf84ce4575a01233394c1d0
sunk1801 <sksunkhurana@gmail.com>
2026-08-19 11:00:01 +0530

    Refactor validator_core.py for improved readability

--- validator_core.py
+++ validator_core.py
@@ -321,14 +321,14 @@ def validate_row(row, master, row_num):
         errors.append("Invalid Last Name")
 
     # Father Name (Mandatory, 2–3 words)
-    if not is_valid_name(val("Father Name"), mandatory=True, min_words=2, max_words=3):
+    if not is_valid_name(val("Father Name"), mandatory=True, min_words=1, max_words=3):
         errors.append("Invalid Father Name")
 
     # Spouse Name (Optional, 2 words)
     if not is_valid_name(
         val("Spouse Name"),
         mandatory=False,
-        min_words=2,
+        min_words=1,
         max_words=3
     ):
         errors.append("Invalid Spouse Name")
